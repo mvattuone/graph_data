518da64400ea3cc5a9cf2b752aca6a8db08a7fbe
Mike Vattuone <mvattuone@gmail.com>
2012-05-05 12:02:24 -0700

    Got the graph working

--- functions.js
+++ functions.js
@@ -1,10 +1,65 @@
-//Todo: Figure out why the graph looks totally wrong.
-//Todo:  Add ticks at the bottom, showing key name rather than associated integer.
+//Todo: Should we normalize this??  How do we do that since there there isn't validation between sections on the single page element?
+
+var lookupTable = ["home_address", "home_zip_code", "has_mailing_address","mailing_address","mailing_city","mailing_state_id","mailing_zip_code", "change_of_address","prev_address","prev_city","prev_state_id","prev_zip_code","name_title","first_name","last_name","change_of_name","prev_name_title","prev_first_name","prev_last_name","id_number","date_of_birth","opt_in_email","phone","phone_type","opt_in_sms","race", "party","us_citizen","finished"];
+				
+var progress_tabs = {phone_type:3,last_name:27,home_address:30,id_number:27,mailing_address:1,opt_in_sms:8,change_of_address:6,first_name:29,prev_zip_code: 4,date_of_birth:39,has_mailing_address:1,party:24,prev_address:4,phone:26,finished:24,opt_in_email:4,mailing_city:1,mailing_zip_code:1,prev_city:4,name_title:30,race:28,prev_state_id:5};
+	
+var progress_accordion = {change_of_address:3,phone_type:1,prev_city:2,last_name:14,date_of_birth:18,prev_address:2,us_citizen:4,id_number:12,name_title:14,prev_zip_code:1,first_name:13,phone:8,finished:8,race:10,prev_state_id:2,home_address:16,party:14,opt_in_email:4};
+	
+var progress_singlepage = {phone_type:15,last_name:36,home_address:36,id_number:35,mailing_address:1,opt_in_sms:4,change_of_address:11,prev_first_name:3,first_name:39,prev_name_title:3,prev_last_name:3,prev_zip_code:7,date_of_birth:41,has_mailing_address:7,party:52,prev_address:7,change_of_name:11,phone:27,finished:27,opt_in_email:8,mailing_city:1,mailing_zip_code:1,prev_city:6,mailing_state:1,name_title:39,home_zip_code:1,race:37,prev_state_id:4};
+
+
 
 $(function() {  
+	
+	//Generates ticks for the x-axis [works]
+	function makeTicks(array) {
+	var ticks = [];
+	for(i=0;i<lookupTable.length;i++) {
+		ticks.push([i+1,lookupTable[i]]);
+	}
+	return ticks;		
+}
+	
+	//Takes in an unsorted object 
+	function lookupCheck(object) {
+		var i = 1;
+		var sortedData = [];
+		for (var prop in lookupTable) {
+			if(object[lookupTable[prop]] === undefined) {
+				sortedData.push([i, 0]);
+			} else {
+				sortedData.push([i, object[lookupTable[prop]]]);
+			}
+			i++;
+		}
+		return sortedData;
+	};
+	
+	
+	var single_data = lookupCheck(progress_singlepage);
+	var tabbed_data = lookupCheck(progress_tabs);
+	var accord_data = lookupCheck(progress_accordion); 
+	
+	
+		
+	//Data Constructor, takes in a label, data, and color
 
-
-	//Settings - This stuff probably gets changed the most, so it goes on top.
+	var Data = function(label, data, color) {
+		this.label = label;
+		this.data = data;
+		this.color = color;
+	}						  
+					  
+	//Let's make our dataplot objects
+	var singleData = new Data("Single Form", single_data, '#409628');
+	var tabbedData = new Data("Tabbed Form", tabbed_data, '#2918DB');
+	var accordionData = new Data("Accordion Form", accord_data, '#988166');	
+	
+	//Make ticks 
+	var ticks = makeTicks(lookupTable);
+	 
+	//Settings
 	var options = {  
 		selection: {
 			 mode: "xy" },
@@ -19,135 +74,32 @@ $(function() {
 	    series: {
 	    	bars: {
 	    		show: true, 
-	    		barWidth: 0.95, 
+	    		barWidth: 0.45, 
 	    		align: 'center'},
 	    },		
 	    xaxis: { 
-	    	ticks: 
-	    		[[1,"change_of_address"],[2,"change_of_name"],
-	    		[3,"date_of_birth"],[4,"finished"],
-	    		[5,"first_name"],[6,"has_mailing_address"],
-	    		[7,"home_address"],[8,"home_zip_code"],
-	    		[9,"id_number"],[10,"last_name"],
-	    		[11,"mailing_address"], [12,"mailing_city"], 
-	    		[13,"mailing_state"], [14,"mailing_zip_code"], 
-	    		[15,"name_title"], [16,"opt_in_email"], 
-	    		[17,"opt_in_sms"], [18,"party"], 
-	    		[19,"phone"], [20,"phone_type"], 
-	    		[21,"prev_address"], [22,"prev_city"], 
-	    		[23,"prev_first_name"], [24,"prev_last_name"], 
-	    		[25,"prev_name_title"], [26,"prev_state_id"], 
-	    		[27,"prev_zip_code"],[28,"race"]]}
-	};  
-	
-	//Lookup Table, specific to Rocky voter reg form fields
-	
-	/*
-	var data = {
-			1: 
-		
-		
-		}
-*/
-
-
-	
-	//This is the part I'm having a hard time with.  How do I get this data to convert into two tuples, with the key set to a corresponding integer ID?
-	//(desired output should be:  [[1, value], [2, value],… [n, value]]; 
-	
-	
-	var progress_tabs = {phone_type:3,last_name:27,home_address:30,id_number:27,mailing_address:1,opt_in_sms:8,change_of_address:6,first_name:29,prev_zip_code: 4,date_of_birth:39,has_mailing_address:1,party:24,prev_address:4,phone:26,finished:24,opt_in_email:4,mailing_city:1,mailing_zip_code:1,prev_city:4,name_title:30,race:28,prev_state_id:5};
-	
-	var progress_accordion = {change_of_address:3,phone_type:1,prev_city:2,last_name:14,date_of_birth:18,prev_address:2,us_citizen:4,id_number:12,name_title:14,prev_zip_code:1,first_name:13,phone:8,finished:8,race:10,prev_state_id:2,home_address:16,party:14,opt_in_email:4};
-	
-	var progress_singlepage = {phone_type:15,last_name:36,home_address:36,id_number:35,mailing_address:1,opt_in_sms:4,change_of_address:11,prev_first_name:3,first_name:39,prev_name_title:3,prev_last_name:3,prev_zip_code:7,date_of_birth:41,has_mailing_address:7,party:52,prev_address:7,change_of_name:11,phone:27,finished:27,opt_in_email:8,mailing_city:1,mailing_zip_code:1,prev_city:6,mailing_state:1,name_title:39,home_zip_code:1,race:37,prev_state_id:4};
-
-
-
-
-	//Sorts JSON objects alphabetically, so we know that we'll have the same IDs for each value no matter what order they are in (TODO: Check for undefined values in case there are missing values from the defined data structure, and insert a 0 or N/A?).
-	
-	function SortArrayByKeys(inputarray) {
-  		var arraykeys=[];
-  		for(var prop in inputarray) {arraykeys.push(prop);}
- 		arraykeys.sort(); 
- 		console.log(arraykeys);
- 		//lookup table
- 		
-	//Now we remove the keys, and return the values in the sorted order.  Yay!
-  		var outputarray=[];
-  for(var i=0; i<arraykeys.length; i++) {
-      outputarray.push(inputarray[arraykeys[i]]);
-  }
-  return outputarray;
-}
-
-	//Store new JSON objects.  Could remove.
-	var single_data_sort = SortArrayByKeys(progress_singlepage);
-	var tabbed_data_sort = SortArrayByKeys(progress_tabs);
-	var accord_data_sort = SortArrayByKeys(progress_accordion);
-	
-
-	//Add integer in place of the original key values.  We know that the numbers will match the alphabetical order of the keys, so we can ensure consistency in our data plotting.
-	var getData = function(object) {
-	var keys = [];
-	var i = 1;
-		for (var prop in object) {	
-			keys.push((i), object[prop]);
-			i++ }
-			return keys;
-	}
-	
-	var single_data = getData(single_data_sort);
-	var tabbed_data = getData(tabbed_data_sort);
-	var accord_data = getData(accord_data_sort);
-	
-	alert(single_data);
-	alert(tabbed_data);
-	alert(accord_data);	  
-
-	//Slice data into smaller arrays and put into large array
-	;
-	var SpliceArray = function(dataArray) {
-	var final_data = []
-	while(dataArray.length) {
-		final_data.push(dataArray.splice(0,2));
-		}
-		return final_data;
-		console.log(final_data);
-	}
-	
-	var single_data = SpliceArray(single_data);
-	var tabbed_data = SpliceArray(tabbed_data);
-	var accord_data = SpliceArray(accord_data);
-	
-	alert(single_data);
-	alert(tabbed_data);
-	alert(accord_data);	  
-	//Data Constructor, takes in a label and data
-
-	var Data = function(label, data, color) {
-		this.label = label;
-		this.data = data;
-		this.color = color;
-	}						  
-					  
-	//Let's make our dataplot objects
-	var singleData = new Data("Single Form", single_data, '#409628');
-	var tabbedData = new Data("Tabbed Form", tabbed_data, '#2918DB');
-	var accordionData = new Data("Accordion Form", accord_data, '#988166');	
-	
-	all_data();
+	    	ticks: ticks
+	    		}
+	  }   
+	  
+	var allData = [singleData, tabbedData, accordionData];
 	 
 	//Set up our graph
+    var allPlot = $("#content");  
+    $.plot( allPlot , allData, options )   
+    
     var singlePlot = $("#content_singlepage");  
-    $.plot( singlePlot , all_data, options )    
+    $.plot( singlePlot , [singleData], options )   
     
     var tabbedPlot = $("#content_tabbed");  
     $.plot( tabbedPlot , [tabbedData] , options )
     
     var accordionPlot = $("#content_accordion");  
     $.plot( accordionPlot , [accordionData] , options ) 
+    
+    //Settings 
+
+
 
 });
     
